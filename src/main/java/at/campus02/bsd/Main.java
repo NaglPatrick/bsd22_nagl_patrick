16a2f7056209ab4948149519a8c08ae1d14c3f59
Patrick Nagl <s51230@edu.campus02.at>
2023-06-01 12:40:54 +0200

    logging configured and implemented in main

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,6 +1,11 @@
 package at.campus02.bsd;
 
+//import java.util.logging.LogManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         double res1 = calc.add(1, 2);
@@ -11,5 +16,8 @@ public class Main {
         System.out.println(res2);
         System.out.println(res3);
         System.out.println(res4);
+
+        logger.error("its an error logger");
+        logger.info("its an info logger");
     }
 }
